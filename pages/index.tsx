d749637d4e0a0e0e1eb9655c9fdbb92218c6b4ae
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-10-05 11:30:59 +0900

    Add @primer/components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import { BaseStyles, Box, Heading } from "@primer/components";
+
 function View() {
-  return <div>Hello Next.js</div>;
+  return (
+    <BaseStyles>
+      <Box m={4}>
+        <Heading mb={2}>Hello, world!</Heading>
+        <p>This will get Primer text styles.</p>
+      </Box>
+    </BaseStyles>
+  );
 }
 
 export default View;
